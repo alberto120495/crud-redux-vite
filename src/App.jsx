9b484e2b8afc98a39ab9216c7960ec4090d8adef
Alberto <albertopimentel04@gmail.com>
2022-05-03 18:17:49 -0500

    Configuracion de Redux

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,24 @@ import Layout from "./layouts/Layout";
 import NuevoProducto from "./pages/NuevoProducto";
 import EditarProducto from "./pages/EditarProducto";
 import Header from "./components/Header";
+
+//Redux
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Productos />} />
-          <Route path="productos/nuevo" element={<NuevoProducto />} />
-          <Route path="productos/editar/:id" element={<EditarProducto />} />
-        </Route>
-      </Routes>
+      <Provider store={store}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Productos />} />
+            <Route path="productos/nuevo" element={<NuevoProducto />} />
+            <Route path="productos/editar/:id" element={<EditarProducto />} />
+          </Route>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
---
+++ src/reducers/productoReducer.jsx
@@ -0,0 +1,12 @@
+const initalState = {
+  productos: [],
+  loading: false,
+  error: null,
+};
+
+export default function (state = initalState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
