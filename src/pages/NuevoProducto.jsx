5170e0caf526623ea7878be0ff2b3742f196727e
Alberto <albertopimentel04@gmail.com>
2022-05-03 16:30:15 -0500

    Formulario nurvo producto

--- src/pages/NuevoProducto.jsx
+++ src/pages/NuevoProducto.jsx
@@ -1,8 +1,40 @@
-
 function NuevoProducto() {
   return (
-    <div>
-      <h1>Nuevo Producto</h1>
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 ">Agregar Nuevo Producto</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="producto">Nombre Producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre del Producto"
+                  name="producto"
+                  id="producto"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="precio">Precio Producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio del Producto"
+                  name="precio"
+                  id="precio"
+                />
+              </div>
+              <input
+                type="submit"
+                value="Agregar "
+                className="btn btn-primary text-uppercase d-block w-100"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
